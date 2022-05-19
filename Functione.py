d438d344bc15dfa0941c6cacc663fa6308541032
Egor <Egorich0270@gmail.com>
2022-05-19 10:40:42 +0800

    beter

--- Functione.py
+++ Functione.py
@@ -1,19 +1,25 @@
-def isEz(a):
+def isezyy(a):
     flag = True
     a = int(a)
-    for x in range(1,(int((a**0.5)+1))):
+    for x in range(1, (int((a**0.5)+1))):
         if (x != 1 and x != a):
             if a%x == 0:
                 flag = False
                 break
     return(flag)
-def simbv(num,sys=10):
+
+def simbv(num, sys=10):
     count=0
     while num // int(sys) != 0:
         count += 1
         num //= int(sys)
     return (count+1)
-def attack(): return 6
-def sleep(): return 'ye'
-def statChec(): return 'good'
+
+def attack(hp):
+    demage = 12
+    return hp-demage
+def sleep(hp):
+    heal = 20
+    return hp+heal
+def statChec(hp): return hp
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Functione import *
 if __name__ == '__main__':
     main = {'1': attack(), '2': sleep(), '3': statChec()}
     while True:
+        HP.
         comand = input('1.Атака\n2.Сон\n3.Статы\n\nВведите команду')
         if comand in main.keys(): print(main.get(comand))
 
