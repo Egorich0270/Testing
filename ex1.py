04b3afb3b816bc3d5b63368bee1b3e6e2a01a55f
Egor <Egorich0270@gmail.com>
2022-05-22 14:32:02 +0800

    new

--- ex1.py
+++ ex1.py
@@ -1,7 +1,14 @@
 class Point:
     color = 'red'
     circle = 2
+    def set_coords(self,x,y):
+        self.x = x
+        self.y = y
+
+    def get_coords(self):
+        return (self.x,self.y)
 
 
 a = Point()
-print(Point.color)
+a.set_coords(10,12)
+print(a.get_coords())
