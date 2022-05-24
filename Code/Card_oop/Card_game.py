57b5fd2087d9ca628a5f3dc272eb9880634b817a
Egor <Egorich0270@gmail.com>
2022-05-24 23:19:52 +0800

    new

--- Code/Card_oop/Card_game.py
+++ Code/Card_oop/Card_game.py
@@ -1,16 +1,26 @@
 import random
+
+
 class Card:
+    card_power_ter = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
 
     def __init__(self, suit, power):
-        self.suit = suit
-        self.power = power
+        self.__suit = suit
+        self.__power = power
+
+    def __add__(self, other):
+        if Card.card_power_ter.get(self.__power) > Card.card_power_ter.get(other.__power):
+            return self.check_card()
+        else:
+            return other.check_card()
 
     def check_card(self):
-        print(f'([{self.suit}] {self.power})', end=' ')
+        print(f'([{self.__suit}] {self.__power})', end=' ')
 
 
 def deck_reader(list):
-    for x in list: x.check_card()
+    for x in list:
+        x.check_card()
 
 
 card_suit = ['♦', '♠', '♥', '♣']
@@ -20,18 +30,26 @@ card_deck_preset = [Card(d, x) for x in card_power for d in card_suit]
 while True:
     card_deck = card_deck_preset
     while len(card_deck) != 0:
-        command = input('\n1)вывести колоду\n2)выбрасить карту\n3)сортировать\n4)перемешать\n5)перегенерировать\n')
+        command = input('\n1)вывести колоду\n2)выбрасить карту\n3)сортировать\n4)перемешать\n5)дуэль\n')
         if command == '1':
             deck_reader(card_deck)
         elif command == '2':
-            num = random.randint(0,len(card_deck)-1)
+            num = random.randint(0, len(card_deck)-1)
             card_deck[num].check_card()
             card_deck.remove(card_deck[num])
-            pass
+
         elif command == '3':
             pass
         elif command == '4':
             random.shuffle(card_deck)
             deck_reader(card_deck)
         elif command == '5':
-            pass
+            num = random.randint(1, len(card_deck) - 1)
+            card_deck[num].check_card()
+            print()
+            print('vs')
+            card_deck[num-1].check_card()
+            print()
+            print(card_deck[num] + card_deck[num-1], 'WIN')
+            card_deck.remove(card_deck[num])
+            card_deck.remove(card_deck[num - 1])
