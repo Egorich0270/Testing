c809cf82bd408d3f12f36c36c542f9c9feea2a65
Egor <Egorich0270@gmail.com>
2022-06-03 11:31:25 +0800

    new

--- Code/Monsters.py
+++ Code/Monsters.py
@@ -1,11 +1,11 @@
 def Monster_leveling(lvl,max_level,cost):
     summa = 0
-    for x in range(lvl,max_level+1):
-        print(f'для апа {x} уровня потребуется {cost*4} еды')
+    for x in range(lvl, max_level):
+        print(f'для апа {x+1} уровня потребуется {cost*4} еды')
         summa += cost * 4
         cost *= 2
 
-    print(f'{summa} еды всего')
+    print(f'{summa} еды всего, это {summa*10} голды')
 
 
 Monster_leveling(int(input('лвл\n')), int(input('нужный левел\n')), int(input('цена апа\n')))
