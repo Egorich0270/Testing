677ed1b464e720f0170102136c5a2a0edd59ce38
Egor <Egorich0270@gmail.com>
2022-06-30 12:10:25 +0800

    cx

--- MyProj/news/models.py
+++ MyProj/news/models.py
@@ -7,3 +7,6 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     id_publesh = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
